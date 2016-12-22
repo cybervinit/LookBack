f1b75a6d6592e21ea0714ce34163445e4c9edf81
Vinit Soni <vinitsoni357@gmail.com>
2016-12-22 01:41:46 -0500

    Camera Partial Implementation

--- app/src/main/java/com/example/memories/lookback/CameraFragment.java
+++ app/src/main/java/com/example/memories/lookback/CameraFragment.java
@@ -1,9 +1,49 @@
 package com.example.memories.lookback;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v13.app.FragmentCompat;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.util.Size;
+import android.util.SparseIntArray;
 import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.jar.Manifest;
 
 /**
  * CameraFragment: This is the fragment that represents the camera screen in the list of
@@ -11,9 +51,93 @@ import android.view.ViewGroup;
  */
 public class CameraFragment extends android.support.v4.app.Fragment {
 
+    private static final int REQUEST_CAMERA_PERMISSION_RESULT = 0;
+    private Context mBaseContext;
+
+
+    // Camera Elements
+    private TextureView mTextureView;
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            Toast.makeText(mBaseContext, "TextureView is available", Toast.LENGTH_SHORT).show();
+            setupCamera(width, height);
+            connectCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
+
+    private CameraDevice mCameraDevice;
+    private CameraDevice.StateCallback mCameraDeviceStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {
+            mCameraDevice = camera;
+            //startPreview();
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            camera.close();
+            mCameraDevice = null;
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            camera.close();
+            mCameraDevice = null;
+        }
+    };
+    private String cameraID;
+    private Size mPreviewSize;
+    private CaptureRequest.Builder mCaptureRequestBuilder;
+    private HandlerThread mBackgroundHandlerThread;
+    private Handler mBackgroundHandler;
+    private static SparseIntArray ORIENTATIONS = new SparseIntArray();
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 0);
+        ORIENTATIONS.append(Surface.ROTATION_90, 90);
+        ORIENTATIONS.append(Surface.ROTATION_180, 180);
+        ORIENTATIONS.append(Surface.ROTATION_270, 270);
+    }
+
+    private MediaRecorder mMediaRecorder;
+    private Button mVideoButton;
+    private boolean isRecording = false;
+
+
+    /**
+     * newInstance: To create a new instance of this CameraFragment.
+     * @return: returns the new intance of the fragment.
+     */
+    public static CameraFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        CameraFragment fragment = new CameraFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mBaseContext = getContext();
+
 
     }
 
@@ -21,21 +145,183 @@ public class CameraFragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.main_fragment, container, false);
+        View view = inflater.inflate(R.layout.camera_fragment, container, false);
+        mVideoButton = (Button) view.findViewById(R.id.video_button);
+        mTextureView = (TextureView) view.findViewById(R.id.texture_view);
+
+        mVideoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return view;
     }
 
-    /**
-     * newInstance: To create a new instance of this CameraFragment.
-     * @return: returns the new intance of the fragment.
-     */
-    public static CameraFragment newInstance() {
-        
-        Bundle args = new Bundle();
 
-        CameraFragment fragment = new CameraFragment();
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void onPause() {
+        closeCamera();
+        stopBackgroundThread();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        startBackgroundThread();
+        if (mTextureView.isAvailable()) {
+            setupCamera(mTextureView.getWidth(), mTextureView.getHeight());
+            connectCamera();
+        } else {
+            mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+        }
+    }
+
+    private void setupCamera(int width, int height) {
+        CameraManager cm = (CameraManager) mBaseContext.getSystemService(Context.CAMERA_SERVICE);
+        try {
+            for (String cameraid : cm.getCameraIdList()) {
+                CameraCharacteristics cChar = cm.getCameraCharacteristics(cameraid);
+                if (cChar.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT) {
+                    StreamConfigurationMap map = cChar.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+
+                    int deviceOrientation = getActivity().getWindowManager().getDefaultDisplay().getRotation();
+                    int totalRotation = sensorToDeviceRotation(cChar, deviceOrientation);
+                    boolean swapRotation = totalRotation == 90 || totalRotation == 270;
+                    int rotatedWidth = width;
+                    int rotatedHeight = height;
+                    if (swapRotation) {
+                        rotatedWidth = height;
+                        rotatedHeight = width;
+                    }
+                    mPreviewSize = chooseOptimalSize(map.getOutputSizes(SurfaceTexture.class), rotatedWidth, rotatedHeight);
+                    cameraID = cameraid;
+                    return;
+                }
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void connectCamera() {
+        CameraManager cm = (CameraManager) getActivity().getSystemService(mBaseContext.CAMERA_SERVICE);
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ContextCompat.checkSelfPermission(mBaseContext, android.Manifest.permission.CAMERA)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    cm.openCamera(cameraID, mCameraDeviceStateCallback, mBackgroundHandler);
+                } else {
+                    if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)) {
+                        Toast.makeText(mBaseContext, "Lookback requires permission to access camera", Toast.LENGTH_SHORT).show();
+                    }
+                    requestPermissions(new String[] {"android.permission.CAMERA"}, REQUEST_CAMERA_PERMISSION_RESULT);
+                }
+            }else {
+                cm.openCamera(cameraID, mCameraDeviceStateCallback, mBackgroundHandler);
+            }
+
+
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startPreview() {
+        SurfaceTexture surfaceTexture = mTextureView.getSurfaceTexture();
+        surfaceTexture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+        Surface previewSurface = new Surface(surfaceTexture);
+
+        try {
+            mCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            mCaptureRequestBuilder.addTarget(previewSurface);
+            mCameraDevice.createCaptureSession(Arrays.asList(previewSurface), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(CameraCaptureSession session) {
+                    try {
+                        session.setRepeatingRequest(mCaptureRequestBuilder.build(), null, mBackgroundHandler);
+                        Toast.makeText(mBaseContext, "Preview configured", Toast.LENGTH_SHORT).show();
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(CameraCaptureSession session) {
+                    Toast.makeText(mBaseContext, "Unable to configure Camera's Preview", Toast.LENGTH_SHORT).show();
+                }
+            }, null);
+
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeCamera() {
+        if (mCameraDevice != null) {
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
+    }
+
+    private void startBackgroundThread () {
+        mBackgroundHandlerThread = new HandlerThread("Camera2VideoImage");
+        mBackgroundHandlerThread.start();
+        mBackgroundHandler = new Handler(mBackgroundHandlerThread.getLooper());
+    }
+
+    private void stopBackgroundThread() {
+        mBackgroundHandlerThread.quitSafely();
+        try {
+            mBackgroundHandlerThread.join();
+            mBackgroundHandlerThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int sensorToDeviceRotation (CameraCharacteristics cChar, int deviceOrientation) {
+        int sensorOrientation = cChar.get(CameraCharacteristics.SENSOR_ORIENTATION);
+        deviceOrientation = ORIENTATIONS.get(deviceOrientation);
+        return (sensorOrientation + deviceOrientation + 360) % 360;
+    }
+
+    private static Size chooseOptimalSize(Size[] choices, int width, int height) {
+        List<Size> bigEnough = new ArrayList<Size>();
+        for (Size option : choices) {
+            if ((option.getHeight() == (option.getWidth() * height / width)) &&
+                    option.getWidth() >= width && option.getHeight() >= height) {
+                bigEnough.add(option);
+            }
+        }
+        if (bigEnough.size() > 0) {
+            return Collections.min(bigEnough, new CompareSizeByArea());
+        }
+        else {
+            return choices[0];
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CAMERA_PERMISSION_RESULT) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(mBaseContext, "Request Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    private static class CompareSizeByArea implements Comparator<Size> {
+
+        @Override
+        public int compare(Size lhs, Size rhs) {
+            return Long.signum((long) lhs.getWidth() * lhs.getHeight() /
+                    (long) rhs.getWidth() * rhs.getHeight());
+        }
     }
 }
--- app/src/main/java/com/example/memories/lookback/MainWrapperActivity.java
+++ app/src/main/java/com/example/memories/lookback/MainWrapperActivity.java
@@ -22,6 +22,7 @@ public class MainWrapperActivity extends FragmentActivity {
         mViewPager.setAdapter(mPagesAdapter);
 
 
+
     }
 
 
